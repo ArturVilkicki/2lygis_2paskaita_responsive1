c78aecf851e9f77e8decbcc802cab45643aac08a
ArturVilkicki <30698622+ArturVilkicki@users.noreply.github.com>
2018-02-06 17:56:13 +0200

    Responsive1

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>2 paskaita Responsive</title>
+	<link href="https://fonts.googleapis.com/css?family=Encode+Sans+Expanded" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container-fluid">
+  		<div class="row pirmoDivoSpalva">
+    		<div class="col-xs-12 col-sm-6 col-lg-8">
+    			<div align="center">
+    				<img src="asmenine.jpg" width="400" height="400">
+    			</div>
+    		</div>
+    		<div class="col-xs-12 col-sm-6 col-lg-4">
+    			<form method="post">
+  				First name:<br>
+  				<input type="text" name="firstname"><br>
+  				Last name:<br>
+  				<input type="text" name="lastname">
+  				<input type="submit" name="submit" value="submit">
+			</form>
+    		</div>
+  		</div>
+  		<div class="row antroDivoSpalva">
+  			<div class="col-xs-12 col-sm-6 col-lg-12">
+  				<h2 align="center">Trumpas aprasymas:</h1>
+  					<div align="center">
+  						<i class="fas fa-address-book"></i>
+  					</div>
+  				
+  				<p class="tekstoSpalva">Esu Artur Vilkicki, man 25 metai!!!!</p>
+  			</div>
+  		</div>
+  		<div class="row trecioDivoSpalva">
+    		<div class="col-xs-12 col-sm-6 col-lg-12">
+    			<h2 align="center">Naujausi darbai:</h1>
+    			<div align="center">
+    				<a href="http://www.delfi.lt" target="_blank" align="center" class="btn btn-primary btn-lg active">Pirmas darbas</a>
+    			</div>
+    			<div align="center">
+    				<a href="http://www.facebook.com" target="_blank" class="btn btn-primary btn-lg active">Antras darbas</a>
+    			</div>
+    			<div align="center">
+    				<a href="http://www.kinozal.tv" target="_blank" class="btn btn-primary btn-lg active">Trecias darbas</a>
+    			</div>
+    			
+    		</div> 
+  		</div>
+  		<div class="row ketvirtoDivoSpalva">
+    		<div class="col-xs-12 col-sm-6 col-lg-12">
+    			<h2 align="center">Kontaktine informacija:</h1>
+    			<ul>
+    				<li class="tekstoSpalva">Vardas Pavarde: Artur Vilkicki</li>
+    				<li class="tekstoSpalva">Kontaktinis telefonas: +370000000</li>
+    				<li class="tekstoSpalva"><a href="http://www.facebook.com" targer="_blank">nuoroda i facebook</li>
+    			</ul>
+    		</div> 
+  		</div>
+	</div>
+</body>
+</html>
